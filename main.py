5576ed95059f468229c5a4aed2facb5fdfbaa235
miguel <migueldelafuente4@gmail.com>
2021-01-18 19:58:57 +0100

    Name changes (runner and sources are common names, confuse IDE) and updates

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ Created on Jan 3, 2021
 @author: Miguel
 '''
 
-from src_taurus_runner.sources import InteractionArgs, WaveFunctionArgs, \
+from src_taurus_runner.tsources import InteractionArgs, WaveFunctionArgs, \
     ParticleNumberArgs, IterationArgs, ConstrainsArgs, InputSource, _Data
-from src_taurus_runner.runner import IsotopeRunner
+from src_taurus_runner.trunner import IsotopeRunner, ConstraintsRunner
 
 
 if __name__ == '__main__':
@@ -27,7 +27,13 @@ if __name__ == '__main__':
     
     ir = IsotopeRunner(z, n_list, interaction)
     ir.runProcess()
-    ir = IsotopeRunner(z, n_list, interaction)
+    
+    _constrains = {ConstrainsArgs.Param.constr_Q20 : [-0.1, 0.0, 0.1, 0.2, 0.3],
+                   ConstrainsArgs.Param.constr_Jz  : 0.5,
+                   ConstrainsArgs.Param.constr_Q10 : [0.5]}
+    ir = ConstraintsRunner(z, n_list, interaction, _constrains)
     ir.runProcess()
+    
+    
     
     
